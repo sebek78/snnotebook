48ea84c2057bfabee2bf76a82818e5503b7bac0e
sebek78 <sebasporek@gmail.com>
2019-11-01 23:54:49 +0100

    add new note to localStorage

--- app/app.module.js
+++ app/app.module.js
@@ -4,10 +4,29 @@
 angular
   .module("snnotebook", ["noteForm", "header", "noteList"])
   .controller("appCtrl", function appCtrl($scope) {
+    const STORAGE_KEY = "snn-notes";
+
+    $scope.getDataFromLocalStorage = function getDataFromLocalStorage() {
+      const data = localStorage.getItem(STORAGE_KEY);
+      if (data !== null) {
+        return data.split(";").map(string => JSON.parse(string));
+      }
+    };
+
     $scope.notes = [];
+    const newNotes = $scope.getDataFromLocalStorage();
+    if (newNotes !== undefined) $scope.notes = [...newNotes];
 
     $scope.addNote = function addNote(text) {
       let newNote = { note: text };
+      let notesCopy = [...this.notes, newNote];
+      let processedNotes = notesCopy
+        .map(obj => {
+          delete obj["$$hashKey"];
+          return JSON.stringify(obj);
+        })
+        .join(";");
+      localStorage.setItem(STORAGE_KEY, processedNotes);
       this.notes.push(newNote);
     };
   });
